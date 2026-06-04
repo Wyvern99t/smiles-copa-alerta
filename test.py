3fc8f5ae6374d6992fad4f4673095b9bc5ad167d
Wyvern99t <juany_99_t@hotmail.com>
2026-06-04 01:27:40 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
+import requests
 
+url = "https://www.smiles.com.ar/emission?originAirportCode=BUE&destinationAirportCode=PUJ&departureDate=2027-04-10&adults=1&children=0&infants=0&isFlexibleDateChecked=false&tripType=1&cabinType=economic&currencyCode=ARS&returnDate=2027-04-18"
+
+r = requests.get(url, timeout=30)
+
+print("Status:", r.status_code)
+print("Longitud HTML:", len(r.text))
+print(r.text[:500])
